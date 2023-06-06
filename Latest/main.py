020594670137d955ec18e26e9f87648666e87b75
AylanurErturk <65497949+AylanurErturk@users.noreply.github.com>
2023-06-06 20:05:12 +0300

    Update main.py
    Counting people enter/exit from one door

--- Latest/main.py
+++ Latest/main.py
@@ -18,16 +18,13 @@ ttl_cntr = 0
 selected = 0
 centroid_dict = defaultdict(list)
 object_id_list = []
-enter = None
-exit = None
+door = None
+
 enter_count = 0
 exit_count = 0
 counted_object_id = []
 outed_object_id = []
 
-def createTracker():
-    tracker = cv2.legacy.TrackerKCF_create()
-    return tracker
 
 def getOutputsNames(net):
     # Get the names of all the layers in the network
@@ -111,9 +108,10 @@ def tracklines(used_boxes):
     global object_id_list
     global centroid_dict
     global enter_count
-    global enter
-    global exit
+    global door
     global exit_count
+    global counted_object_id
+    global outed_object_id
     objects = tracker.update(used_boxes)
     for (objectId, newbox) in objects.items():
         x1, y1, x2, y2 = newbox
@@ -142,14 +140,51 @@ def tracklines(used_boxes):
                     start_pt = (centroid_dict[objectId][pt][0], centroid_dict[objectId][pt][1])
                     end_pt = (centroid_dict[objectId][pt + 1][0], centroid_dict[objectId][pt + 1][1])
                     cv2.line(frame, start_pt, end_pt, (0, 255, 0), 4)
-                    
-        if enter and exit is not None:      
-            enter_count, exit_count = countPeople(frame, enter, exit, newbox, objectId)
+        #object count starts from here:            
+        if door is not None:      
+            #enter_count, exit_count = countPeople(frame, door, newbox, objectId)
+            enx1, eny1, enx2, eny2 = door
+            mideny = int(eny1 + eny2 / 2)
+            midenx = int(enx2 + enx1)
+            #line start x: enx1
+            #line start y: mideny
+            #line end x= midenx
+            #line end y: mideny 
             
-        
+            cv2.line(frame, (enx1, mideny), (midenx, mideny), (255, 0, 0), 2)
+            
+            l = len(centroid_dict[objectId])
+            
+            for pt in range(len(centroid_dict[objectId])):
+               if not pt + 1 == l:
+                    if enx1 < start_pt[0] and start_pt[0] < midenx:
+                        if mideny > start_pt[1] and mideny <= end_pt[1] and objectId not in outed_object_id:
+                            print("çık")
+                            exit_count += 1
+                            outed_object_id.append(objectId)
+                            print(objectId)
+                        if mideny < start_pt[1] and mideny >= end_pt[1] and objectId not in counted_object_id:
+                	        print("gir")
+                	        enter_count += 1
+                	        counted_object_id.append(objectId)
+                	        print(objectId)
+                    else:
+                        continue
+            
+            
+            
+                    
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
         text = "ID: {}".format(objectId)
         cv2.putText(frame, text, (x1, y1-5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 1)
+        
+        
+        
+     
+    
+    
+        
+        
     cv2.putText(frame, "Enters: " + str(enter_count), (0, 150), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
     cv2.putText(frame, "Exits: " + str(exit_count), (0, 180), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
     cv2.putText(frame, "# inside " + str(enter_count - exit_count), (0, 210), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
@@ -161,81 +196,17 @@ def selectEntrance(frame):
     cv2.setWindowProperty('Select the entrance', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
     # Select entrance ROI
-    enter = cv2.selectROI('Select the entrance', frame)
+    door = cv2.selectROI('Select the entrance', frame)
 
     # Destroy the entrance window
     cv2.destroyAllWindows()
 
     # Create resizable window and set properties
-    cv2.namedWindow('Select the exit', cv2.WINDOW_NORMAL)
-    cv2.setWindowProperty('Select the exit', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-
-    # Select exit ROI
-    exit = cv2.selectROI('Select the exit', frame)
-
-    # Destroy the exit window
-    cv2.destroyAllWindows()
-
-    return enter, exit
 
 
-def countPeople(frame, enter, exit, rect, objectId):
-    global enter_count
-    global exit_count
-    global counted_object_id
-    global outed_object_id
-    rx1, ry1, rx2, ry2 = rect
-    enx1, eny1, enx2, eny2 = enter
-    ex1, ey1, ex2, ey2 = exit
-    
-    rx1 = int(rx1)
-    ry1 = int(ry1)
-    rx2 = int(rx2)
-    ry2 = int(ry2)
-    
-    enx1 = int(enx1)
-    eny1 = int(eny1)
-    enx2 = int(enx2) + enx1
-    eny2 = int(eny2) + eny1
-    
-    
-    ex1 = int(ex1)
-    ey1 = int(ey1)
-    ex2 = int(ex2) + ex1
-    ey2 = int(ey2) + ey1
-    
-   #r1 inside r2
-    
-    if rx1 > enx1 and ry1 > eny1 and rx2 < enx2 and ry2 < eny2 and objectId not in counted_object_id:
-        enter_count += 1
-        counted_object_id.append(objectId)
-        print(objectId, "   Entered, +")
-    if rx1 > ex1 and ry1 > ey1 and rx2 < ex2 and ry2 < ey2 and objectId not in outed_object_id:
-        exit_count += 1
-        outed_object_id.append(objectId)
-        print(objectId, "Exit, -")
-        
-    #if ry1 < eny2:
-    #    count += 1
-    #    print("c2")
-    #if enx1 < rx1 < enx2 < rx2 and ry1 < eny2:
-    #   count += 1
-    #    print("c3")
-    
-    
-    #if ey1 < ry2:
-    #    count -= 1
-    #    print("c4")
-    
-    #if rx1 < ex1 < rx2 < ex2 and ey1 < ry2:
-    #    count -= 1
-    #    print("c5")
-    #if ex1 < rx1 < ex2 < rx2 and ey1 < ry2:
-    #    count -= 1
-    #    print("c6")
-        
 
-    return enter_count, exit_count
+
+    return door
 
         
 
@@ -301,10 +272,9 @@ while cv2.waitKey(1) < 0:
     # Remove the bounding boxes with low confidence
     used_boxes = postprocess(frame, outs)
     tracklines(used_boxes)
-    while exit is None:
-        enter, exit = selectEntrance(frame)
-    cv2.rectangle(frame, (enter[0], enter[1]), (enter[0] + enter[2], enter[1] + enter[3]), (0, 0, 255), 2)
-    cv2.rectangle(frame, (exit[0], exit[1]), (exit[0] + exit[2], exit[1] + exit[3]), (0, 0, 255), 2)
+    while door is None:
+        door = selectEntrance(frame)
+    cv2.rectangle(frame, (door[0], door[1]), (door[0] + door[2], door[1] + door[3]), (0, 0, 255), 2)
     
     
     # Write the frame with the detection boxes
